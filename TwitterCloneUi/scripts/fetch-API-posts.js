dc0abab917da97563c85a4db6207cf9f7a29049c
Carl Alcantara <carlfrancis.alcantara@gmail.com>
2024-02-11 23:14:00 +0800

    Created a fetch api that will display the posts of the user logged in

--- TwitterCloneUi/scripts/fetch-API-posts.js
+++ TwitterCloneUi/scripts/fetch-API-posts.js
@@ -19,3 +19,44 @@ async function postCreatePost() {
         console.log('failed');
     }
 }
+
+async function getPosts() {
+    let token = localStorage.getItem("token");
+
+    const res = await fetch('http://localhost:3000/api/v1/posts', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    })
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        data.forEach(user => {
+            const posts = user.content;
+            console.log(posts)
+
+            // document.getElementById("posts-container").insertAdjacentHTML('beforeend', posts);
+
+            var postDiv = document.createElement("div");
+            postDiv.className = "post"
+            var username = "Cosmos";
+
+            postDiv.innerHTML = '<div class="post-username">' + username + '</div><div class="post-text">' + posts + '</div>';
+
+            document.querySelector(".timeline-placeholder").appendChild(postDiv);
+        })
+    })
+    .catch(error => console.log(error));
+    
+
+    // if (res.ok) {
+    //     console.log('successful');
+    //     posts = res.json();
+    //     console.log(posts);
+    // } else {
+    //     console.log('failed');
+    // }
+}
